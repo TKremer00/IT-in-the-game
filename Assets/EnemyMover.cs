67d187b8425ad3a96d5795db60e225b9f07cfe2d
Tim <tim7voetbal@live.nl>
2020-12-04 14:11:17 +0100

    Made enemy move to target

---
+++ Assets/EnemyMover.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    public Transform target; // The end position to walk to
+    public float speed = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position != target.position){
+            MoveToVector(target.position);
+        }else {
+            // Enemy is at the target
+            Die();
+        }
+    }
+
+    // Let the enemy walk towards a target
+    public void MoveToVector(Vector3 target) 
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position,target,step);
+        transform.LookAt(this.target);
+    }
+    
+    // The destory the enemy
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
