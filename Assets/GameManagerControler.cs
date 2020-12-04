9cb8e3e3c97f8f509c2eae0494a6476bb0342714
Indonimus_wolf <jeroenschraa@gmail.com>
2020-12-04 11:41:45 +0100

    added money

---
+++ Assets/GameManagerControler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerControler : MonoBehaviour
+{
+
+    float money = 0.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SetMoney (float money) {
+        this.money = money;
+    }
+    
+    float GetMoney () {
+        return money;
+    }
+}
