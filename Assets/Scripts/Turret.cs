93aaea4b41c2f9dc7f6296a5ab109ff049bf8708
MagicPlatypus <heikebrakert@hotmail.com>
2020-12-16 16:50:44 +0100

    Turret now got bullets

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-    public Transform target;
+    private GameObject target;
+
+    [Header("Attributes")]
     public float range = 3f;
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+
+    [Header("Unity Setup Fields")]
+
     public string enemyTag = "Enemy";
+
+    public Transform partToRotate;
+    public float turnSpeed = 10f;
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+
+    
+    
+
 
     // Start is called before the first frame update
     void Start()
@@ -31,7 +48,10 @@ public class Turret : MonoBehaviour
 
         if (nearestEnemy != null && shortestDistance <= range)
         {
-            target = nearestEnemy.transform;
+            target = nearestEnemy.transform.position;
+       } else
+       {
+           target = null;
        }
     }
 
@@ -40,8 +60,33 @@ public class Turret : MonoBehaviour
     {
         if (target == null)
         {
-           return;
+            return;
+        }
+           
+       
+
+       Vector3 dir = target.position - transform.position;
+       Quaternion lookRotation = Quaternion.LookRotation(dir);
+       Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+       partToRotate.rotation = Quaternion.Euler (0f, rotation.y, 0f);
+
+       if (fireCountdown <= 0f)
+       {
+           Shoot();
+           fireCountdown = 1f / fireRate;
        }
+    }
+
+    void Shoot ()
+    {
+        GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+        if(bullet != null)
+        {
+            bullet.Seek(target);
+        }
+
     }
 
     void OnDrawGizmosSelected () 
