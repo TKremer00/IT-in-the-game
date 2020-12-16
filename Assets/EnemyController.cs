3a773652d45f4dc371f24fcfbc9896461089b731
Tim <tim7voetbal@live.nl>
2020-12-16 16:18:56 +0100

    changed map layout

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -31,9 +31,10 @@ public class EnemyController : MonoBehaviour
         float step = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position,target,step);
         // Let enemy look at target
-        Quaternion targetRotation = Quaternion.LookRotation( target );
-        targetRotation.x = 0;
-        transform.rotation = Quaternion.Lerp(transform.rotation,targetRotation, Time.deltaTime);
+        
+        // Quaternion targetRotation = Quaternion.LookRotation( target );
+        // targetRotation.x = 0;
+        // transform.rotation = Quaternion.Lerp(transform.rotation,targetRotation, Time.deltaTime);
     }
 
     // add the money to the users account
